b3b8af25d808bb6898b7278a9cc45f1ad304814b
Otavio Pliger <me@otaviopliger.com>
2018-11-08 12:07:39 +0700

    Remove useless code

--- index.js
+++ index.js
@@ -3,20 +3,13 @@ const execa = require('execa')
 const run = args => execa('blkid', args)
     .then(response =>
         response.stdout.split('\n').map(x => {
-            path = x.match(/^(\S+)\:/)[1]
-            uuid = (x.match(/\bUUID\b\=\"([^\"]+)\"/) || [null])[1]
-            type = (x.match(/\bTYPE\b\=\"([^\"]+)\"/) || [null])[1]
-            label = (x.match(/\bLABEL\b\=\"([^\"]+)\"/) || [null])[1]
-            partuuid = (x.match(/\bPARTUUID\b\=\"([^\"]+)\"/) || [null])[1]
-            partlabel = (x.match(/\bPARTLABEL\b\=\"([^\"]+)\"/) || [null])[1]
-
             return {
-                path: path,
-                uuid: uuid,
-                type: type,
-                label: label,
-                partuuid: partuuid,
-                partlabel: partlabel
+                path: (x.match(/^(\S+)\:/) || [null])[1],
+                uuid: (x.match(/\bUUID\b\=\"([^\"]+)\"/) || [null])[1],
+                type: (x.match(/\bTYPE\b\=\"([^\"]+)\"/) || [null])[1],
+                label: (x.match(/\bLABEL\b\=\"([^\"]+)\"/) || [null])[1],
+                partuuid: (x.match(/\bPARTUUID\b\=\"([^\"]+)\"/) || [null])[1],
+                partlabel: (x.match(/\bPARTLABEL\b\=\"([^\"]+)\"/) || [null])[1]
             }
         })
     )
